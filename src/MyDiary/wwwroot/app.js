4c1aff8b76513d16b5a8b925e0210830231b5a44
PKudupu <Prathap.bolar@gmail.com>
2016-03-10 13:03:47 -0500

    using repeat-for

--- src/MyDiary/wwwroot/app.js
+++ src/MyDiary/wwwroot/app.js
@@ -1,11 +1,22 @@
-﻿export class App {
+﻿import {HttpClient} from "aurelia-http-client";
+
+export class App {
     constructor () {
         this.message = "";
+        this.http = new HttpClient();
     }
     activate() {
         this.message = "calling from activate.";
+        return this.http.get("/movies.json")
+                            .then(response=> {
+                                this.movies = response.content;
+
+            });
+                      
+    
     }
     ChangeMessage() {
         this.message = " Button click change message.";
+       
     }
 }
