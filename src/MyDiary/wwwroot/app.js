d5b863c372c1c8ff68f1cbcf9e9db27d7ee4e6e0
PKudupu <Prathap.bolar@gmail.com>
2016-03-31 09:08:34 -0400

    List friends

--- src/MyDiary/wwwroot/app.js
+++ src/MyDiary/wwwroot/app.js
@@ -1,7 +1,18 @@
-﻿export class App {
-    constructor()
-    {
-       this.message="Hello boston code camp"
+﻿export class App{
+    constructor() {
+        this.firstName = "Rob";
+        this.lastName = "Eisenberg";
+        this.friends = [];
+        this.potentialFriend = '';
+    }
+    get fullName() {
+        return `${this.firstName}${this.lastName}`;
+    }
+    addfriend() {
+         if (this.potentialFriend) {
+            this.friends.push(this.potentialFriend);
+        }
+        this.potentialFriend = '';
     }
     
 }
